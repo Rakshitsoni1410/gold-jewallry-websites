51e49167abd591e5434105c894bc5de61c21ae51
Rakshitson1410 <rakshitrsoni@gmail.com>
2024-10-04 15:13:10 +0530

    ok

--- login.php
+++ login.php
@@ -28,7 +28,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         if ($stmt->affected_rows > 0) {
             // Registration successful, redirect to home page
-            header('Location: index.php');
+            header('Location: index.html');
             exit;
         } else {
             $error = 'Registration failed';
