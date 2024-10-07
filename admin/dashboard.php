239bdda41856f722ccc6f513b3033062182530da
Rakshitson1410 <rakshitrsoni@gmail.com>
2024-10-07 17:36:30 +0530

    ok

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -116,7 +116,7 @@ try {
         </div>
     </div>
 
-    <!-- Payment Modal -->
+    <!-- Payment Modal --><!-- Payment Modal -->
 <div class="modal fade" id="paymentModal" tabindex="-1" role="dialog" aria-labelledby="paymentModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
