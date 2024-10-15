e3e076317bc41878e50278ae961bb7e50b557db2
Rakshitson1410 <rakshitrsoni@gmail.com>
2024-10-15 18:52:33 +0530

    ok

--- pendals for woman.php
+++ pendals for woman.php
@@ -1,6 +1,7 @@
 <?php
 // Enable error reporting
-
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 $servername = "localhost";
 $username = "root"; // Default username
@@ -49,22 +50,22 @@ $conn->close();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Jewelry Collection</title>
-    <link rel="stylesheet" href="RINGT.CSS"> <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="RINGT.CSS"> 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script type="text/javascript" src="workforwishlist.js" async></script>
-    <script src="addbutton.js"async></script>
+    <script src="addbutton.js" async></script>
 </head>
 
+<body>
 <div class="head">
-    <h2>Gold woman PENDALS</h2>
+    <h2>Gold Woman Pendals</h2>
     <div class="icon">
         <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal">
-            <h2 style="justify-items: auto;" class="work">
-                <i class="fa-solid fa-heart" style="color: #e60abe;"></i>
-            </h2>
+            <i class="fa-solid fa-heart" style="color: #e60abe;"></i>
         </button>
 
-        <!-- Modal -->
+        <!-- Wishlist Modal -->
         <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
@@ -86,7 +87,7 @@ $conn->close();
         <a href="index.html"><i class="fa-solid fa-house" style="color: #080707;"></i></a>
     </div>
 
-    <!-- Modal -->
+    <!-- Cart Modal -->
     <div class="modal fade" id="cartModal" tabindex="-1" aria-labelledby="cartModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
@@ -106,33 +107,39 @@ $conn->close();
 <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-inner">
         <div class="carousel-item active">
-            <img src="https://static.malabargoldanddiamonds.com/media/catalog/category/gold-pendant.jpg" class="d-block w-100" width="100px">
+            <img src="https://static.malabargoldanddiamonds.com/media/catalog/category/gold-pendant.jpg" class="d-block w-100" alt="Carousel Image">
         </div>
     </div>
 </div>
 
-<div class="box">
-    <div class="wrapper">
-<div class="card-container">
-    <?php foreach ($data as $item): ?>
-        <div class="card">
-            <img src="<?php echo $item['image']; ?>" alt="Jewelry Image">
-            <p class="title"><?php echo $item['description']; ?></p>
-            <p class="available-carat">Available Carats: 
-                <ul>
-                    <?php foreach ($item['carats'] as $carat): ?>
-                        <li><?php echo $carat; ?></li>
-                    <?php endforeach; ?>
-                </ul>
-            </p>
-            <div class="button-wrap">
-                <a href="#" class="cart button" id="add-to-cart"><i class="fa-solid fa-cart-shopping"></i></a>
-                <a href="#" class="wish button" id="wishlist"><i class="fa fa-heart"></i></a>
+<div class="container mt-4">
+    <div class="row">
+        <?php foreach ($data as $item): ?>
+            <div class="col-md-3 mb-4"> <!-- 4 cards per row on medium screens and above -->
+                <div class="card">
+                    <img src="<?php echo $item['image']; ?>" alt="Jewelry Image" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo $item['description']; ?></h5>
+                        <p class="available-carat">Available Carats:
+                            <ul>
+                                <?php foreach ($item['carats'] as $carat): ?>
+                                    <li><?php echo $carat; ?></li>
+                                <?php endforeach; ?>
+                            </ul>
+                        </p>
+                        <div class="button-wrap">
+                            <a href="#" class="cart button" id="add-to-cart"><i class="fa-solid fa-cart-shopping"></i></a>
+                            <a href="#" class="wish button" id="wishlist"><i class="fa fa-heart"></i></a>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-    <?php endforeach; ?>
+        <?php endforeach; ?>
+    </div>
 </div>
-</div>
-</div>
+
+<!-- Bootstrap JS and dependencies -->
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-2R8I6jZbMkZ6k8C6X5CfFZ0NF5hKj2a4YkK5lpuSQ4LPC2k6F1pmh8bxBOH4D1D6" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-S2uH4G4x4OG3bZ5Qp4N2OHgiq1HIkqMSjsOWHA8h3I7qhrAdHSKgh9Fkp2mW7xXx" crossorigin="anonymous"></script>
 </body>
 </html>
